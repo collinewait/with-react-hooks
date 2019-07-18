4d5d2c83c2ed7e219b8d4d447efb75b7ff3ce3dd
collinewait <collinewait17@gmail.com>
2019-07-18 10:05:49 +0300

    use the effect hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  
+  /**
+   * Similar to componentDidMount and componentDidUpdate:
+   * This function can optionally return a function that
+   * performs clean up as shown bellow:
+   * useEffect(() => {
+   * document.title = `You clicked ${count} times`;
+   * return () => console.log('clean up executed');
+  });
+   */
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <div>
